f77bda28600284326ba5cd0f30cd7f7704ce4d01
Eugene Duvenage <eugene@eugeneduvenage.com>
2014-05-08 21:04:37 +0200

    #6 Implement DEMA Indicator

--- indicators/dema.go
+++ indicators/dema.go
@@ -3,7 +3,84 @@ package indicators
 
 // DEMA(X) = (2 * EMA(X, CLOSE)) - (EMA(X, EMA(X, CLOSE)))
 
-// import (
-// 	"github.com/thetruetrade/gotrade"
-// 	"math"
-// )
+import (
+	"github.com/thetruetrade/gotrade"
+)
+
+type baseDEMA struct {
+	*baseIndicatorWithLookback
+
+	// private variables
+	valueAvailableAction ValueAvailableAction
+	ema1                 *EMA
+	ema2                 *EMA
+	currentEMA           float64
+}
+
+func newBaseDEMA(lookbackPeriod int) *baseDEMA {
+	newDEMA := baseDEMA{baseIndicatorWithLookback: newBaseIndicatorWithLookback(lookbackPeriod)}
+	return &newDEMA
+}
+
+// A Double Exponential Moving Average Indicator
+type DEMA struct {
+	*baseDEMA
+
+	// public variables
+	Data []float64
+}
+
+// NewDEMA returns a new Double Exponential Moving Average (DEMA) configured with the
+// specified lookbackPeriod. The DEMA results are stored in the DATA field.
+func NewDEMA(lookbackPeriod int, selectData gotrade.DataSelectionFunc) (indicator *DEMA, err error) {
+	newDEMA := DEMA{baseDEMA: newBaseDEMA(lookbackPeriod)}
+	newDEMA.ema1, _ = NewEMA(lookbackPeriod, selectData)
+
+	newDEMA.ema1.valueAvailableAction = func(dataItem float64, streamBarIndex int) {
+		newDEMA.currentEMA = dataItem
+		newDEMA.ema2.ReceiveTick(dataItem, streamBarIndex)
+	}
+
+	newDEMA.ema2, _ = NewEMA(lookbackPeriod, selectData)
+
+	newDEMA.ema2.valueAvailableAction = func(dataItem float64, streamBarIndex int) {
+		newDEMA.dataLength += 1
+		if newDEMA.validFromBar == -1 {
+			newDEMA.validFromBar = streamBarIndex
+		}
+
+		// DEMA(X) = (2 * EMA(X, CLOSE)) - (EMA(X, EMA(X, CLOSE)))
+		dema := (2 * newDEMA.currentEMA) - dataItem
+
+		if dema > newDEMA.maxValue {
+			newDEMA.maxValue = dema
+		}
+
+		if dema < newDEMA.minValue {
+			newDEMA.minValue = dema
+		}
+
+		newDEMA.valueAvailableAction(dema, streamBarIndex)
+	}
+
+	newDEMA.selectData = selectData
+	newDEMA.valueAvailableAction = func(dataItem float64, streamBarIndex int) {
+		newDEMA.Data = append(newDEMA.Data, dataItem)
+	}
+	return &newDEMA, nil
+}
+
+func NewDEMAForStream(priceStream *gotrade.DOHLCVStream, lookbackPeriod int, selectData gotrade.DataSelectionFunc) (indicator *DEMA, err error) {
+	newDEMA, err := NewDEMA(lookbackPeriod, selectData)
+	priceStream.AddTickSubscription(newDEMA)
+	return newDEMA, err
+}
+
+func (dema *baseDEMA) ReceiveDOHLCVTick(tickData gotrade.DOHLCV, streamBarIndex int) {
+	var selectedData = dema.selectData(tickData)
+	dema.ReceiveTick(selectedData, streamBarIndex)
+}
+
+func (dema *baseDEMA) ReceiveTick(tickData float64, streamBarIndex int) {
+	dema.ema1.ReceiveTick(tickData, streamBarIndex)
+}
--- indicators/dema_test.go
+++ indicators/dema_test.go
@@ -1,1 +1,94 @@
 package indicators_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/thetruetrade/gotrade"
+	. "github.com/thetruetrade/gotrade/indicators"
+	"time"
+)
+
+var _ = Describe("when calculating a double exponential moving average (dema)", func() {
+	var (
+		period     int = 3
+		dema       *DEMA
+		indicator  Indicator
+		sourceData = []gotrade.DOHLCV{gotrade.NewDOHLCVDataItem(time.Now(), 0.0, 0.0, 0.0, 5.0, 0.0),
+			gotrade.NewDOHLCVDataItem(time.Now(), 0.0, 0.0, 0.0, 6.0, 0.0),
+			gotrade.NewDOHLCVDataItem(time.Now(), 0.0, 0.0, 0.0, 7.0, 0.0),
+			gotrade.NewDOHLCVDataItem(time.Now(), 0.0, 0.0, 0.0, 8.0, 0.0),
+			gotrade.NewDOHLCVDataItem(time.Now(), 0.0, 0.0, 0.0, 9.0, 0.0),
+			gotrade.NewDOHLCVDataItem(time.Now(), 0.0, 0.0, 0.0, 10.0, 0.0),
+			gotrade.NewDOHLCVDataItem(time.Now(), 0.0, 0.0, 0.0, 11.0, 0.0),
+			gotrade.NewDOHLCVDataItem(time.Now(), 0.0, 0.0, 0.0, 12.0, 0.0),
+			gotrade.NewDOHLCVDataItem(time.Now(), 0.0, 0.0, 0.0, 13.0, 0.0)}
+	)
+
+	BeforeEach(func() {
+		dema, _ = NewDEMA(period, gotrade.UseClosePrice)
+		indicator = dema
+	})
+
+	Context("and the dema has received less ticks than twice the lookback period", func() {
+
+		BeforeEach(func() {
+			for i := 0; i < period*2-2; i++ {
+				dema.ReceiveDOHLCVTick(sourceData[i], i+1)
+			}
+		})
+
+		It("the dema should have no result data", func() {
+			Expect(len(dema.Data)).To(Equal(0))
+		})
+
+		It("the indicator stream length should be zero", func() {
+			Expect(dema.Length()).To(Equal(0))
+		})
+	})
+
+	Context("and the dema has received ticks equal to twice the lookback period", func() {
+
+		BeforeEach(func() {
+			for i := 0; i <= 2*period-2; i++ {
+				dema.ReceiveDOHLCVTick(sourceData[i], i+1)
+			}
+		})
+
+		It("the dema should have result data with a single entry", func() {
+			Expect(len(dema.Data)).To(Equal(1))
+		})
+
+		It("the indicator stream length should be one", func() {
+			Expect(dema.Length()).To(Equal(1))
+		})
+
+		It("the indicator stream min and max should be equal", func() {
+			Expect(dema.MaxValue()).To(Equal(dema.MinValue()))
+		})
+
+		It("the indicator stream should have valid bars from twice the lookback period", func() {
+			Expect(indicator.ValidFromBar()).To(Equal(2*period - 1))
+		})
+	})
+
+	Context("and the dema has received more ticks than the lookback period", func() {
+
+		BeforeEach(func() {
+			for i := range sourceData {
+				dema.ReceiveDOHLCVTick(sourceData[i], i+1)
+			}
+		})
+
+		It("the dema should have result data with entries equal to the number of ticks less twice the (lookback period - 2)", func() {
+			Expect(len(dema.Data)).To(Equal(len(sourceData) - (2*period - 2)))
+		})
+
+		It("the indicator stream min should equal the result data minimum", func() {
+			Expect(indicator.MinValue()).To(Equal(GetDataMin(dema.Data)))
+		})
+
+		It("the indicator stream max should equal the result data maximum", func() {
+			Expect(indicator.MaxValue()).To(Equal(GetDataMax(dema.Data)))
+		})
+	})
+})
