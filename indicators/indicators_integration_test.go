508820c37283081803044292fe8240e5b46cf859
Eugene Duvenage <eugene@eugeneduvenage.com>
2014-05-31 22:38:57 +0200

    closes #52 Implement Median Price Indicator

--- indicators/indicators_integration_test.go
+++ indicators/indicators_integration_test.go
@@ -613,4 +613,38 @@ var _ = Describe("when executing the gotrade truerange with a years data and kno
 			})
 		})
 	})
+
+	var _ = Describe("when executing the gotrade median price indicator with a years data and known output", func() {
+		var (
+			medPrice        *indicators.MedPrice
+			expectedResults []float64
+			err             error
+			priceStream     *gotrade.DOHLCVStream
+		)
+
+		BeforeEach(func() {
+			// load the expected results data
+			expectedResults, _ = LoadCSVPriceDataFromFile("medprice_expectedresult.data")
+			priceStream = gotrade.NewDOHLCVStream()
+		})
+
+		Describe("using no lookback period", func() {
+
+			BeforeEach(func() {
+				medPrice, err = indicators.NewMedPrice()
+				priceStream.AddTickSubscription(medPrice)
+				csvFeed.FillDOHLCVStream(priceStream)
+			})
+
+			It("the result set should have a length equal to the source data length", func() {
+				Expect(medPrice.Length()).To(Equal(len(priceStream.Data)))
+			})
+
+			It("it should have correctly calculated the median price for each item in the result set accurate to two decimal places", func() {
+				for k := range expectedResults {
+					Expect(expectedResults[k]).To(BeNumerically("~", medPrice.Data[k], 0.01))
+				}
+			})
+		})
+	})
 })
---
+++ indicators/medprice.go
@@ -0,0 +1,62 @@
+package indicators
+
+import (
+	"github.com/thetruetrade/gotrade"
+)
+
+type MedPriceWithoutStorage struct {
+	*baseIndicator
+
+	// private variables
+	valueAvailableAction ValueAvailableAction
+}
+
+func NewMedPriceWithoutStorage(valueAvailableAction ValueAvailableAction) (indicator *MedPriceWithoutStorage, err error) {
+	newVar := MedPriceWithoutStorage{baseIndicator: newBaseIndicator()}
+
+	newVar.valueAvailableAction = valueAvailableAction
+
+	return &newVar, nil
+}
+
+type MedPrice struct {
+	*MedPriceWithoutStorage
+
+	// public variables
+	Data []float64
+}
+
+func NewMedPrice() (indicator *MedPrice, err error) {
+	newVar := MedPrice{}
+	newVar.MedPriceWithoutStorage, err = NewMedPriceWithoutStorage(func(dataItem float64, streamBarIndex int) {
+		newVar.Data = append(newVar.Data, dataItem)
+	})
+
+	return &newVar, err
+}
+
+func NewMedPriceForStream(priceStream *gotrade.DOHLCVStream) (indicator *MedPrice, err error) {
+	newVar, err := NewMedPrice()
+	priceStream.AddTickSubscription(newVar)
+	return newVar, err
+}
+
+func (ind *MedPriceWithoutStorage) ReceiveDOHLCVTick(tickData gotrade.DOHLCV, streamBarIndex int) {
+	ind.dataLength += 1
+
+	if ind.validFromBar == -1 {
+		ind.validFromBar = streamBarIndex
+	}
+
+	result := (tickData.H() + tickData.L()) / float64(2.0)
+
+	if result > ind.maxValue {
+		ind.maxValue = result
+	}
+
+	if result < ind.minValue {
+		ind.minValue = result
+	}
+
+	ind.valueAvailableAction(result, streamBarIndex)
+}
