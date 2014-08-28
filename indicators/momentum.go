c76fd68b450ab2c54a63deef8a68b6bce76509ec
Eugene Duvenage <eugene@eugeneduvenage.com>
2014-08-28 19:57:11 +0200

    #76 Remove duplication - momentum

--- indicators/momentum.go
+++ indicators/momentum.go
@@ -8,14 +8,12 @@ import (
 
 // A Momentum Indicator (Mom), no storage, for use in other indicators
 type MomWithoutStorage struct {
-	*baseIndicator
-	*baseFloatBounds
+	*baseIndicatorWithFloatBounds
 
 	// private variables
-	valueAvailableAction ValueAvailableActionFloat
-	periodCounter        int
-	periodHistory        *list.List
-	timePeriod           int
+	periodCounter int
+	periodHistory *list.List
+	timePeriod    int
 }
 
 // NewMomWithoutStorage creates a Momentum Indicator (Mom) without storage
@@ -38,12 +36,10 @@ func NewMomWithoutStorage(timePeriod int, valueAvailableAction ValueAvailableAct
 
 	lookback := timePeriod
 	ind := MomWithoutStorage{
-		baseIndicator:        newBaseIndicator(lookback),
-		baseFloatBounds:      newBaseFloatBounds(),
-		periodCounter:        (timePeriod * -1),
-		periodHistory:        list.New(),
-		valueAvailableAction: valueAvailableAction,
-		timePeriod:           timePeriod,
+		baseIndicatorWithFloatBounds: newBaseIndicatorWithFloatBounds(lookback, valueAvailableAction),
+		periodCounter:                (timePeriod * -1),
+		periodHistory:                list.New(),
+		timePeriod:                   timePeriod,
 	}
 
 	return &ind, err
@@ -145,26 +141,9 @@ func (ind *MomWithoutStorage) ReceiveTick(tickData float64, streamBarIndex int) 
 		// Mom = price - previousPrice
 		previousPrice := ind.periodHistory.Front().Value.(float64)
 
-		// increment the number of results this indicator can be expected to return
-		ind.dataLength += 1
-		if ind.validFromBar == -1 {
-			// set the streamBarIndex from which this indicator returns valid results
-			ind.validFromBar = streamBarIndex
-		}
 		var result float64 = tickData - previousPrice
 
-		// update the maximum result value
-		if result > ind.maxValue {
-			ind.maxValue = result
-		}
-
-		// update the minimum result value
-		if result < ind.minValue {
-			ind.minValue = result
-		}
-
-		// notify of a new result value though the value available action
-		ind.valueAvailableAction(result, streamBarIndex)
+		ind.UpdateIndicatorWithNewValue(result, streamBarIndex)
 	}
 
 	if ind.periodHistory.Len() > ind.timePeriod {
